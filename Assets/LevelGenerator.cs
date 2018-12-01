a84ef0c86615a752aa676398f619c0da250a48ff
Azargaz <huba310@gmail.com>
2018-12-01 10:30:57 +0100

    Added player movement and level generator.

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+	public Texture2D[] maps;
+	public ColorToPrefab[] colorMappings;
+
+	public Transform cameraTransform;
+
+	void Start () 
+	{		
+		cameraTransform.position = new Vector3(Mathf.FloorToInt(maps[0].width/2), Mathf.FloorToInt(maps[0].height/2), -15);
+		
+		Vector2 offset = Vector2.zero;
+		foreach (Texture2D map in maps)
+		{
+			GenerateLevel(map, offset);
+			offset += new Vector2(0, map.height);
+		}
+	}
+
+	void GenerateLevel(Texture2D map, Vector2 offset)
+	{
+		for (int x = 0; x < map.width; x++)
+		{
+			for (int y = 0; y < map.height; y++)
+			{
+				Color pixelColor = map.GetPixel(x, y);
+				if(pixelColor.a != 0)
+				{					
+					GenerateTile(x, y, pixelColor, offset);
+				}
+			}
+		}
+	}
+
+	void GenerateTile(int x, int y, Color pixelColor, Vector2 offset)
+	{
+		foreach (ColorToPrefab colorMapping in colorMappings)
+		{
+			if (colorMapping.color.Equals(pixelColor))
+			{
+				Vector2 position = new Vector2(x, y) + offset;
+				Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class MovementController : MonoBehaviour
+{
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    public float moveSpeed = 6;
+
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    Controller2D controller;
+    Animator anim;
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    void Update()
+    {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (input.y > 0 && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        anim.SetFloat("walk", Mathf.Abs(input.x));
+        anim.SetBool("grounded", controller.collisions.below);
+        sprite.flipX = input.x == 0 ? sprite.flipX : input.x < 0;
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
